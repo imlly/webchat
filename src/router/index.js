8a4c35d6561dd810dfcdb7a98142517875ebd991
504574335 <44646576+504574335@users.noreply.github.com>
2020-04-19 22:21:48 +0800

    Vue前端
    实现了登录注册、聊天页面数据返回

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import chat from '@/components/chat'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: chat
     }
   ]
 })
