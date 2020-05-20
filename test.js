4ccfe422ddc0b86ad9cb1961ebbb8e8d44812930
504574335 <44646576+504574335@users.noreply.github.com>
2020-05-20 13:50:28 +0800

    test

--- test.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-let server = http.createServer(function(request, response){
-response.writeHead(200, {'Content-Type':'text/plain'});
-response.end("hello world\n");
-
-}).listen(8081);
-
-console.log('server running');
