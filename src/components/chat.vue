faa42842667ac7f95c6f55515cfa3d6c45521a2c
504574335 <44646576+504574335@users.noreply.github.com>
2020-05-22 11:42:47 +0800

    debug

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -236,7 +236,6 @@ export default {
         myHead:'',
         nickname:'',
         newNick:'',
-        messageList: [],
         // 搜索相关
         userList:[],
         linkmanList:[],
@@ -356,6 +355,7 @@ export default {
             //console.log(data.result);
             //console.log(data.result.length);
             self.messageList = data.result;
+            // console.log(moment(data.result[0].createdAt).utcOffset(480).format('YYYY-MM-DD HH:mm:ss'));
             //提取消息列表好友头像
             for(var i=0; i<self.messageList.length; i++){
                 self.messageList[i].message = self.obj.replaceFace(self.messageList[i].message);
@@ -533,7 +533,7 @@ export default {
                     console.log(JSON.stringify(error));
                     console.log(error.result);
                 }).finally(function() {
-                console.log('请求最近消息列表成功');
+                    console.log('请求最近消息列表成功');
                 });
             }
             else if(index==1)
